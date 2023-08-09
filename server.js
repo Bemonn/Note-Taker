e4a17c2b4f3e34153dcf6b951d87ca530f3336e1
Bemon <tristan2107b@gmail.com>
2023-08-10 06:08:07 +1000

    Added delete route and commented on code

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+//Fetch all notes from db.json
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
@@ -26,18 +27,19 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
+//Add a new note to db.json
 app.post('/api/notes', (req, res) => {
     const newNote = { ...req.body, id: uuidv4() };
 
-    // Read the current notes from db.json
+    //Read the current notes from db.json
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
 
-        // Add the new note to the list
+        //Add the new note to the list
         notes.push(newNote);
 
-        // Write the updated list of notes back to db.json
+        //Write the updated list of notes back to db.json
         fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
             if (err) throw err;
             res.json(newNote);
@@ -45,10 +47,32 @@ app.post('/api/notes', (req, res) => {
     });
 });
 
+//Delete a note using uuid from db.json
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    //Read the current notes from db.json
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+
+        //Filter out the note with the given id
+        notes = notes.filter(note => note.id !== noteId);
+
+        //Write the updated list of notes back to db.json
+        fs.writeFile('./db/db.json', JSON.stringify(notes, null, 2), (err) => {
+            if (err) throw err;
+            res.json({ message: `Note with ID: ${noteId} has been deleted.` });
+        });
+    });
+});
+
+//Default route for index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//Start the server
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
